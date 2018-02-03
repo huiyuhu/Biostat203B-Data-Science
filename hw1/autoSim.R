a690c27ecfcbc1f0ff7adcbaf86a9b8f93f17d13
huiyuhu <huiyuhu@ucla.edu>
2018-02-03 07:13:47 +0000

    modify format for R

--- hw1/autoSim.R
+++ hw1/autoSim.R
@@ -2,15 +2,16 @@
 rep <- 50
 seed <- 280
 distTypes = c("gaussian", "t1", "t5")
-nVals = seq(100, 500, by=100)
+nVals = seq(100, 500, by = 100)
 
 for (n in nVals) {
   for (dist in distTypes) {
     oFile = paste("n", n, "_", dist, ".txt", sep="")
-    arg = paste("seed=", seed," n=", n," dist=\\\"", dist,"\\\" rep=",rep, sep="")
+    arg = paste("seed=", seed, " n=", n, " dist=\\\"", dist, "\\\" rep=",
+                rep, sep = "")
     sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile)
     system(sysCall)
-    print(paste("sysCall=", sysCall, sep=""))
+    print(paste("sysCall=", sysCall, sep = ""))
   }
 }
 
